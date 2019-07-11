b77f58541c019528d7ed41d876dbe59251f723d0
Zhao TengHu <z.th.btc@gmail.com>
2019-07-11 08:22:27 -0700

    Integrate ontology

--- src/services/derive/tron.js
+++ src/services/derive/tron.js
@@ -4,30 +4,16 @@
 import TronWeb from 'tronweb';
 import bip39 from 'bip39';
 import * as bip32 from 'bip32';
-
 import { Buffer } from 'safe-buffer';
 
-const BIP44 = {
-  INDEX: 195,
-  HEXA: 0x800000c3
-};
+const BIP44_INDEX = 195;
 
-function getAccountAtIndex(mnemonic, index = 0) {
+export function deriveTron(mnemonic, index=0) {
   const seed = bip39.mnemonicToSeedHex(mnemonic);
   const node = bip32.fromSeed(new Buffer(seed, 'hex'));
-  const child = node.derivePath(`m/44'/${BIP44.INDEX}'/${index}'/0/0`, seed);
+  const child = node.derivePath(`m/44'/${BIP44_INDEX}'/${index}'/0/0`, seed);
   const privateKey = child.privateKey.toString('hex');
   const address = TronWeb.address.fromPrivateKey(privateKey);
 
-  return {
-    privateKey,
-    address
-  };
-}
-
-
-
-export function deriveTron(mnemonic) {
-  const { address, privateKey } = getAccountAtIndex(mnemonic, 0);
   return { address, privateKey, publicKey: '' };
 }
