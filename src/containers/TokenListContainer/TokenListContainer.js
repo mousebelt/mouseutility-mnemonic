7760844758045061c69965679d4b97eb15fee2fb
Zhao TengHu <z.th.btc@gmail.com>
2019-07-11 08:38:54 -0700

    Remove unused package import

--- src/containers/TokenListContainer/TokenListContainer.js
+++ src/containers/TokenListContainer/TokenListContainer.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import bip39 from 'bip39';
-import { utils } from 'ontology-ts-sdk'
 import { Row, Col, Input, Button, Layout } from 'antd';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
